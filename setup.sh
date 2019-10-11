2e6237ef4d75e34b3a868cfc8e6e8b8d1a1d0c61
renzo <renzoarreaza@gmail.com>
2019-10-11 12:23:12 +0000

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -5,13 +5,17 @@ for file in $config_files
 do
 	ln -s -f $PWD/$file $HOME/
 done
+
 if [ "$TERM" != "xterm-256color" ]
 then
-	echo 'export TERM="xterm-256color"' >> ~/.bashrc
-	echo ".bashrc has been updated. Reload using one of the lines below:"
-	echo ""
-	echo -e "source ~/.bashrc"
-	echo -e ". ~/.bashrc"
-	echo -e "exec bash"
-	
+	if [[ "$SHELL" == *"bash"* ]]
+	then
+		echo 'export TERM="xterm-256color"' >> ~/.bashrc
+		echo ".bashrc has been updated. Reload using one of the lines below:"
+		echo ""
+		echo -e "source ~/.bashrc"
+		echo -e ". ~/.bashrc"
+		echo -e "exec bash"
+	fi
+	# zsh
 fi
